3f4b70e55dfee418cb681eb515a914103c144053
Vojtěch Frič <you@example.com>
2020-11-12 21:07:41 +0100

    added demo to read opt3001 sensor

---
+++ opt3001/src/main.cpp
@@ -0,0 +1,106 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <ClosedCube_OPT3001.h>
+
+ClosedCube_OPT3001 opt3001;
+
+#define OPT3001_ADDRESS 0x44
+void printError(String text, OPT3001_ErrorCode error) {
+	Serial.print(text);
+	Serial.print(": [ERROR] Code #");
+	Serial.println(error);
+}
+
+void printResult(String text, OPT3001 result) {
+	if (result.error == NO_ERROR) {
+		//Serial.print(text);
+		//Serial.print(": ");
+		Serial.println(result.lux);
+		//Serial.println(" lux");
+	}
+	else {
+		printError(text,result.error);
+	}
+}
+
+void configureSensor() {
+	OPT3001_Config newConfig;
+	
+	newConfig.RangeNumber = B1100;	
+	newConfig.ConvertionTime = B1;
+	newConfig.Latch = B1;
+	newConfig.ModeOfConversionOperation = B11;
+
+	OPT3001_ErrorCode errorConfig = opt3001.writeConfig(newConfig);
+	if (errorConfig != NO_ERROR)
+		printError("OPT3001 configuration", errorConfig);
+	else {
+		OPT3001_Config sensorConfig = opt3001.readConfig();
+		Serial.println("OPT3001 Current Config:");
+		Serial.println("------------------------------");
+		
+		Serial.print("Conversion ready (R):");
+		Serial.println(sensorConfig.ConversionReady,HEX);
+
+		Serial.print("Conversion time (R/W):");
+		Serial.println(sensorConfig.ConvertionTime, HEX);
+
+		Serial.print("Fault count field (R/W):");
+		Serial.println(sensorConfig.FaultCount, HEX);
+
+		Serial.print("Flag high field (R-only):");
+		Serial.println(sensorConfig.FlagHigh, HEX);
+
+		Serial.print("Flag low field (R-only):");
+		Serial.println(sensorConfig.FlagLow, HEX);
+
+		Serial.print("Latch field (R/W):");
+		Serial.println(sensorConfig.Latch, HEX);
+
+		Serial.print("Mask exponent field (R/W):");
+		Serial.println(sensorConfig.MaskExponent, HEX);
+
+		Serial.print("Mode of conversion operation (R/W):");
+		Serial.println(sensorConfig.ModeOfConversionOperation, HEX);
+
+		Serial.print("Polarity field (R/W):");
+		Serial.println(sensorConfig.Polarity, HEX);
+
+		Serial.print("Overflow flag (R-only):");
+		Serial.println(sensorConfig.OverflowFlag, HEX);
+
+		Serial.print("Range number (R/W):");
+		Serial.println(sensorConfig.RangeNumber, HEX);
+
+		Serial.println("------------------------------");
+	}
+	
+}
+
+
+
+void setup() {
+  delay(5000);
+  
+	Serial.begin(9600);
+	Serial.println("ClosedCube OPT3001 Arduino Test");
+
+  delay(3000);
+
+	opt3001.begin(OPT3001_ADDRESS);
+	Serial.print("OPT3001 Manufacturer ID");
+	Serial.println(opt3001.readManufacturerID());
+	Serial.print("OPT3001 Device ID");
+	Serial.println(opt3001.readDeviceID());
+
+	configureSensor();
+	printResult("High-Limit", opt3001.readHighLimit());
+	printResult("Low-Limit", opt3001.readLowLimit());
+	Serial.println("----");
+}
+
+void loop() {
+  OPT3001 result = opt3001.readResult();
+	printResult("OPT3001", result);
+	delay(1000);
+}
